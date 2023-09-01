9499b88a86946134402d54c54de0b4e94ff04139
dattavijit <37737599+dattavijit@users.noreply.github.com>
2023-09-01 16:00:25 +0930

    Modules

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['John', 'Beth', 'Mike', 'Mary', 'Sue', 'Tina'];
+const ages = [20, 25, 30, 35, 40, 45];
+console.log(people);
+
+module.exports = {
+    people: people,
+    ages: ages
+}
