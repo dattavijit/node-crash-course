0191fb686b9c799dd9c76da00abb76f02ff00a64
dattavijit <37737599+dattavijit@users.noreply.github.com>
2023-09-01 23:26:17 +0930

    Return HTML from the server

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader("content-type", "text/html");
+
+    //  res.write('<h1> hello, ninjas </h1>');
+    //  res.write('<p>hello again, ninjas</p>');
+    //  res.end();
+
+    //  send an HTML file using fs.
+    fs.readFile("./views/index.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
+server.listen(3000, "localhost", () => {
+    console.log("listening for requests on port 3000");
 });
